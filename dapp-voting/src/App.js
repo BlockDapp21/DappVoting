53aaa727be989c21676607987dff16af4e4e2198
Blockdapp21 <blockdapp21@gmail.com>
2023-09-28 10:59:20 +0200

    Initial commit

--- dapp-voting/src/App.js
+++ dapp-voting/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './pages/Navbar';
 import Home from './pages/Home.js';
 import Vote from './pages/Vote.js';
@@ -10,46 +10,38 @@ import { ethers } from "ethers";
 const VOTEAddress = process.env.REACT_APP_VOTE_ADDRESS;
 
 function App() {
+    const [account, setAccount] = useState(null);
+    const [vote, setVote] = useState({});
+    const [loading, setLoading] = useState(true);
 
-  const [account, setAccount] = useState(null);
-  const [vote, setVote] = useState({}); //{}= objet vide
-  const [loading, setLoading] = useState(true);
+    const loadBlockchainData = async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const voteContract = new ethers.Contract(VOTEAddress, VOTE.abi, provider);
+        setVote(voteContract);
+        setLoading(false);
+    }
 
-  const loadBlockchainData = async () => {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    setAccount(accounts[0]);
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const vote = new ethers.Contract(VOTEAddress, VOTE.abi, signer);
-    setVote(vote);
-    setLoading(false);
-  }
+    useEffect(() => {
+        loadBlockchainData();
+    }, []);
 
-  useEffect(() => {
-    loadBlockchainData()
-  }, [])
-
-  return (
-    <BrowserRouter>
-      <div className="App">
-          <Navbar account={account} setAccount={setAccount} />
-        <div>
-        {loading ? (
-            <div>Loading...</div>
-          ) : (
-          <Routes>
-            <Route path="/" element={
-              <Home vote={vote} />
-            } /> 
-            <Route path="/vote/:id" element={
-              <Vote vote={vote} />
-            } /> 
-          </Routes>
-          )}
-        </div>
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Navbar account={account} setAccount={setAccount} setVote={setVote} VOTEAddress={VOTEAddress} VOTE={VOTE}/>
+                <div>
+                    {loading ? (
+                        <div>Loading...</div>
+                    ) : (
+                        <Routes>
+                            <Route path="/" element={<Home vote={vote} />} /> 
+                            <Route path="/vote/:id" element={<Vote vote={vote} />} /> 
+                        </Routes>
+                    )}
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
